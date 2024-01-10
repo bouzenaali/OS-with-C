c227806c4b7b5f67cfa7ffb643168618e7a62861
bouzenaali <alibouzena16@gmail.com>
2024-01-10 20:36:25 +0100

    fix: fork creates only one child

--- test2/fork.c
+++ test2/fork.c
@@ -4,23 +4,33 @@
 
 int main(int argc, char *argv[]){
     printf("I am the process p1 THE PARENT\n");
+    // here we are at the parent process
     int id1 = fork();
 
     if (id1 == 0){
+        // here we are at the first child (first gen) 
         printf("I am the process p2 SON OF p1\n");
 
         int id2 = fork();
 
         if (id2 == 0){
+            // here we are at the first child (second gen)
             printf("I am the process p4 SON OF p2\n");
         } 
     } else {
-        printf("I am the process p3 SON OF p1\n");
+        // still at the parent process
 
         int id3 = fork();
 
         if (id3 == 0){
-            printf("I am the process p5 SON OF p3\n");
+            // here we are at the second child (first gen)
+            printf("I am the process p3 SON OF p1\n");
+            int id4 = fork();
+
+            if (id4 == 0){
+                // here we are at the second child (sencod gen)
+                printf("I am the process p5 SON OF p3\n");   
+            }
         } 
     };
     return 0;
---
+++ test2/fork.md
@@ -0,0 +1,9 @@
+#
+## 
+```text
+I am the process p1 THE PARENT
+I am the process p3 SON OF p1
+I am the process p2 SON OF p1
+I am the process p5 SON OF p3
+I am the process p4 SON OF p2
+```
