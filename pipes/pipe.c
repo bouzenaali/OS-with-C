d370e5c3c4e1f41fb9d4d81d008650ed7c7df0d5
bouzenaali <alibouzena16@gmail.com>
2024-01-16 17:31:45 +0100

    feat: add a simple  one way pipes script

---
+++ pipes/pipe.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]){
+    int fd[2];
+    // fd[0] - read
+    // fd[1] - write
+
+    if (pipe(fd)==-1){
+        printf("an error occured while creating a pipe\n");
+    }
+
+    int id = fork();
+    if (id == 0){
+        close(fd[0]);
+        int x;
+        printf("Input a number: ");
+        scanf("%d",&x);
+        write(fd[1], &x, sizeof(int));
+        close(fd[1]);
+    } else {
+        close(fd[1]);
+        int y;
+        read(fd[0], &y, sizeof(int));
+        close(fd[0]);
+        printf("Got from the child %d\n",y);
+    }
+
+    return 0;
+}
